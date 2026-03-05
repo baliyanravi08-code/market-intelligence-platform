b88d759d1f635877f42c74793e9300aa37657f33
Ravi Baliyan <baliyanravi08@gmail.com>
2026-03-05 20:59:40 +0530

    serve react dashboard from express

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const http = require("http");
+const path = require("path");
 const { Server } = require("socket.io");
 
 const startBSEListener = require("./services/listeners/bseListener");
@@ -16,19 +17,39 @@ const io = new Server(server, {
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Market Intelligence Platform Running 🚀");
+/*
+SERVE REACT BUILD
+*/
+
+const clientPath = path.join(__dirname, "../client/dist");
+
+app.use(express.static(clientPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(clientPath, "index.html"));
 });
+
+/*
+SOCKET CONNECTION
+*/
 
 io.on("connection", (socket) => {
   console.log("Client connected:", socket.id);
 });
 
+/*
+START LISTENERS
+*/
+
 startBSEListener(io);
 startNSEDealsListener(io);
+
+/*
+START SERVER
+*/
 
 const PORT = process.env.PORT || 10000;
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
