3f3653012d05a8d4796391daea85292fa0106e46
Ravi Baliyan <baliyanravi08@gmail.com>
2026-03-07 00:51:23 +0530

    fix duplicated axios declaration in bseListener

--- server/services/listeners/bseListener.js
+++ server/services/listeners/bseListener.js
@@ -5,6 +5,7 @@ const analyzeAnnouncement = require("../analyzers/announcementAnalyzer");
 const orderBookEngine = require("../intelligence/orderBookEngine");
 const orderStrengthEngine = require("../intelligence/orderStrengthEngine");
 const orderMomentumEngine = require("../intelligence/orderMomentumEngine");
+
 const sectorRadar = require("../intelligence/sectorRadar");
 const sectorBoomEngine = require("../intelligence/sectorBoomEngine");
 
@@ -52,7 +53,7 @@ async function fetchAnnouncements(){
 
     console.log("📢 BSE Announcements fetched:",list.length);
 
-    const alerts=[];
+    const alerts = [];
 
     for(const item of list){
 
@@ -84,15 +85,11 @@ async function fetchAnnouncements(){
 
       if(!signal) continue;
 
-      /* attach pdf link */
-
       signal.pdfUrl = announcement.pdfUrl;
 
       alerts.push(signal);
 
       updateRadar(signal.company,signal);
-
-      /* ORDER BOOK ENGINE */
 
       const orderData = orderBookEngine(signal);
 
@@ -117,10 +114,10 @@ async function fetchAnnouncements(){
         if(sectorData && sectorData.orders >=3){
 
           ioRef.emit("sector_alerts",[{
-            sector:sectorData.sector,
-            orders:sectorData.orders,
-            companies:sectorData.companies,
-            totalValue:sectorData.totalValue
+            sector: sectorData.sector,
+            orders: sectorData.orders,
+            companies: sectorData.companies,
+            totalValue: sectorData.totalValue
           }]);
 
         }
@@ -135,159 +132,7 @@ async function fetchAnnouncements(){
 
     }
 
-    if(alerts.length>0 && ioRef){
-
-      ioRef.emit("market_events",alerts);
-
-    }
-
-  }
-  catch(err){
-
-    console.log("❌ BSE Feed Failed:",err.message);
-
-  }
-
-}
-
-module.exports = startBSEListener;const axios = require("axios");
-
-const analyzeAnnouncement = require("../analyzers/announcementAnalyzer");
-
-const orderBookEngine = require("../intelligence/orderBookEngine");
-const orderStrengthEngine = require("../intelligence/orderStrengthEngine");
-const orderMomentumEngine = require("../intelligence/orderMomentumEngine");
-const sectorRadar = require("../intelligence/sectorRadar");
-const sectorBoomEngine = require("../intelligence/sectorBoomEngine");
-
-const { updateRadar } = require("../intelligence/radarEngine");
-
-let ioRef = null;
-let seen = new Set();
-
-function startBSEListener(io){
-
-  ioRef = io;
-
-  console.log("🚀 BSE Listener running...");
-
-  fetchAnnouncements();
-
-  setInterval(fetchAnnouncements,30000);
-
-}
-
-async function fetchAnnouncements(){
-
-  try{
-
-    const url =
-      "https://api.bseindia.com/BseIndiaAPI/api/AnnSubCategoryGetData/w";
-
-    const res = await axios.get(url,{
-      params:{
-        pageno:1,
-        strCat:-1,
-        strPrevDate:"",
-        strScrip:"",
-        strSearch:"P",
-        strToDate:"",
-        strType:"C"
-      },
-      headers:{
-        "User-Agent":"Mozilla/5.0",
-        "Referer":"https://www.bseindia.com/"
-      }
-    });
-
-    const list = res.data?.Table || [];
-
-    console.log("📢 BSE Announcements fetched:",list.length);
-
-    const alerts=[];
-
-    for(const item of list){
-
-      const company = item.SLONGNAME;
-      const code = item.SCRIP_CD;
-      const title = item.HEADLINE;
-      const date = item.NEWS_DT;
-
-      const id = code + title + date;
-
-      if(seen.has(id)) continue;
-
-      seen.add(id);
-
-      const announcement = {
-
-        company,
-        code,
-        title,
-        date,
-
-        pdfUrl: item.ATTACHMENTNAME
-          ? `https://www.bseindia.com/xml-data/corpfiling/AttachLive/${item.ATTACHMENTNAME}`
-          : null
-
-      };
-
-      const signal = await analyzeAnnouncement(announcement);
-
-      if(!signal) continue;
-
-      /* attach pdf link */
-
-      signal.pdfUrl = announcement.pdfUrl;
-
-      alerts.push(signal);
-
-      updateRadar(signal.company,signal);
-
-      /* ORDER BOOK ENGINE */
-
-      const orderData = orderBookEngine(signal);
-
-      if(orderData){
-
-        ioRef.emit("order_book_update",orderData);
-
-        const strength = orderStrengthEngine(signal.code);
-
-        if(strength){
-          ioRef.emit("order_strength",strength);
-        }
-
-        const momentum = orderMomentumEngine(signal);
-
-        if(momentum){
-          ioRef.emit("order_momentum",momentum);
-        }
-
-        const sectorData = sectorRadar(signal);
-
-        if(sectorData && sectorData.orders >=3){
-
-          ioRef.emit("sector_alerts",[{
-            sector:sectorData.sector,
-            orders:sectorData.orders,
-            companies:sectorData.companies,
-            totalValue:sectorData.totalValue
-          }]);
-
-        }
-
-        const boom = sectorBoomEngine(signal);
-
-        if(boom){
-          ioRef.emit("sector_boom",boom);
-        }
-
-      }
-
-    }
-
-    if(alerts.length>0 && ioRef){
+    if(alerts.length > 0 && ioRef){
 
       ioRef.emit("market_events",alerts);
 
